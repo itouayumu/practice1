5f17d2a85ee0f88c65eba861021a923ac9e174ee
itou <a.itou.sys22@morijyobi.ac.jp>
2022-09-27 10:35:43 +0900

    pra2.javaに100回出力処理追記

--- pra2.java
+++ pra2.java
@@ -1,9 +1,13 @@
-public class Hello{
-	 public static void  main(String[] args) {
-		
-		if(int i=o;i<100;i++){
 
-		 System.out.println("Hello World"); 
+public class pra2{
+	 public static void  main(String[] args) {
+		for (int i = 0; i < 100; i++){
+
+		System.out.println("hello wprld");
 		}
+		
 	 }
 }
+	
+	 
+
